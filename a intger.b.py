08eccdcad41f6db7e51bfed6e9f2ed2e4ab31399
walelgnatanaw <118797543+walelgnatanaw@users.noreply.github.com>
2022-11-22 20:58:45 +0200

    do ex

---
+++ a intger.b.py
@@ -0,0 +1,27 @@
+print(0+1)
+print(50)
+print("50")
+print(50+50)
+print(3+5)
+x=9
+y=5
+print ( x-y)
+a=23
+b=10
+c=67
+d=50
+x=a+b
+y=c-d
+print("the sum and sub is," ,str(x) ,str(y))
+print(type(2.33))
+num1=int(input ("the firest number"))
+num2=int(input("the 2cd number"))
+sum=int(num1)+int(num2)
+print( "the sum of the input is",str(sum))
+print (5.2+2.4)
+print ("5.4+3.4")
+print(5)
+num1=float(input ("the firest number"))
+num2=float(input("the 2cd number"))
+sum=float(num1)*int(num2)
+print( "the product of the input is",str(sum))
